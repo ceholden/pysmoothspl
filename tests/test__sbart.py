22842ac81e3502bf9269731e4913fbaf8e585393
Chris Holden <ceholden@gmail.com>
2017-06-07 16:15:00 -0400

    Modify tests to check only yhat for now

--- tests/test__sbart.py
+++ tests/test__sbart.py
@@ -8,11 +8,13 @@ from pysmoothspl import sbart
                                 'spline_timeseries_1'))
 def test_success_timeseries(df, request):
     df = request.getfixturevalue(df)
-    yhat = sbart(
+    knots, coef, yhat, x_min, x_range = sbart(
         df['x'].values,
         df['y'].values,
         df['w'].values,
         0.2
     )
     # Check output is _something_ for now...
+    # TODO: check all outputs against R, either known values
+    #       or by running rpy2
     assert np.isfinite(yhat).all()
