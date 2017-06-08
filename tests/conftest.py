a0f420e4e14a257b8a7d11b6e49e4186c3d43392
Chris Holden <ceholden@gmail.com>
2017-06-08 10:15:17 -0400

    Add test data expected values & how to calc them
    If rpy2, the tests will actually run R code to figure
    out the expected smoothing spline fits

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,23 +4,75 @@ import numpy as np
 import pandas as pd
 import pytest
 
+try:
+    import rpy2  # noqa
+    from rpy2.robjects.packages import importr
+    from rpy2.robjects import numpy2ri, pandas2ri
+
+    print('rpy2')
+    numpy2ri.activate()
+    pandas2ri.activate()
+
+    Rstats = importr('stats')
+except Exception:
+    HAS_RPY2 = False
+else:
+    HAS_RPY2 = True
+
 HERE = Path(__file__).parent
 
+SEED = 12345
+SPAR_VALUES = (0.1, 0.2, 0.5, 0.75, 1.0, )
 
-@pytest.fixture()
+
+@pytest.fixture(scope='module')
 def evi_timeseries_1():
+    """ pd.DataFrame with columns x, y, w and smoothed answers in "spl_{spar}"
+    """
     csv = str(HERE.joinpath('data', 'evi_timeseries_1.csv'))
-    return pd.read_csv(csv)
+
+    if HAS_RPY2:
+        # Calculate and overwrite, helping to track for changes in how R
+        # calculates the values (i.e., because git would show modified)
+        df = pd.read_csv(csv)
+        df_ans = _rpy2_smooth_spline(df, SPAR_VALUES)
+        df_ans.to_csv(csv, index=False)
+        return df_ans
+    else:
+        return df
 
 
-@pytest.fixture()
-def spline_timeseries_1():
-    n = 1000
-    x = np.arange(n).astype(float)
-    y = 100 + 50 * np.sin(x) + np.random.normal(0, 5, n)
-    w = np.ones_like(x)
-    return pd.DataFrame({
-        'x': x,
-        'y': y,
-        'w': w
-    })
+@pytest.fixture(scope='module')
+def sine_timeseries_1():
+    csv = str(HERE.joinpath('data', 'sinwave_with_noise_seed12345.csv'))
+
+    if HAS_RPY2:
+        np.random.seed(SEED)
+        n = 1000
+        x = np.arange(n).astype(float)
+        y = 100 + 50 * np.sin(x) + np.random.normal(0, 5, n)
+        w = np.ones_like(x)
+        df = pd.DataFrame({
+            'x': x,
+            'y': y,
+            'w': w
+        })
+        df_ans = _rpy2_smooth_spline(df, SPAR_VALUES)
+        df_ans.to_csv(csv)
+        return df_ans
+    else:
+        return pd.read_csv(csv)
+
+    return df_ans
+
+
+def _rpy2_smooth_spline(df, spars):
+    for spar in spars:
+        spl = Rstats.smooth_spline(df['x'].values,
+                                   df['y'].values,
+                                   df['w'].values,
+                                   spar=spar)
+        cname = 'spl_{spar:02f}'.format(spar=spar)
+        df[cname] = Rstats.predict_smooth_spline(spl, df['x'].values)[1]
+
+    return df
--- tests/test__sbart.py
+++ tests/test__sbart.py
@@ -5,16 +5,22 @@ from pysmoothspl import sbart
 
 
 @pytest.mark.parametrize('df', ('evi_timeseries_1',
-                                'spline_timeseries_1'))
+                                'sine_timeseries_1'))
 def test_success_timeseries(df, request):
     df = request.getfixturevalue(df)
-    knots, coef, yhat, x_min, x_range = sbart(
-        df['x'].values,
-        df['y'].values,
-        df['w'].values,
-        0.2
-    )
-    # Check output is _something_ for now...
-    # TODO: check all outputs against R, either known values
-    #       or by running rpy2
-    assert np.isfinite(yhat).all()
+
+    # df has answers in it named "spl_{spar}"
+    cnames = [c for c in df.columns if 'spl_' in c]
+    for cname in cnames:
+        yhat_ans = df[cname]
+        spar = float(cname.split('_')[1])
+
+        knots, coef, yhat, x_min, x_range = sbart(
+            df['x'].values,
+            df['y'].values,
+            df['w'].values,
+            spar
+        )
+
+        assert np.isfinite(yhat).all()
+        assert np.allclose(yhat, yhat_ans)
